2350b96b438326f3c8972d5cdc6f409b91e02ceb
Niklas W <niklas.weingarz@gmail.com>
2019-09-14 13:54:04 +0200

    Work in progress: CaseBase Creation
    Added CaseBase class which is a modified version of a dataset that each case handler will use to store its cases
    
    Changed data set creation so it will also safe the feature names and store the examples ordered by its case

--- case_based_similarity/CaseBase.py
+++ case_based_similarity/CaseBase.py
@@ -2,49 +2,59 @@ import numpy as np
 from sklearn import preprocessing
 
 from configuration.Configuration import Configuration
-from neural_network.DatasetEncoder import DatasetEncoder
 
 
+# TODO Completely untested
 class Dataset:
 
-    def __init__(self, dataset_folder, config: Configuration, training):
+    def __init__(self, dataset_folder, config: Configuration, case, features_used=None):
         self.dataset_folder = dataset_folder
         self.config: Configuration = config
 
         self.x_train = None
         self.y_train = None
-        self.x_test = None
-        self.y_test = None
+
+        self.case = case
+
+        self.feature_names_all = None
+        self.features_used = features_used
+        self.indices_features = None
 
         self.one_hot_encoder = None
 
         self.num_train_instances = None
-        self.num_test_instances = None
         self.num_instances = None
         self.time_series_length = None
         self.time_series_depth = None
         self.num_classes = None
         self.classes = None
 
-        if self.config.architecture_variant in ['standard_simple', 'standard_ffnn'] or training:
-            pass  # nothing to do if standard variant
-        elif self.config.architecture_variant in ['fast_simple', 'fast_ffnn']:
-            print('Fast SNN variant configured, encoding the dataset with subnet ...')
-            encoder = DatasetEncoder(self.dataset_folder, config)
-            encoder.encode()
-            self.dataset_folder = encoder.target_folder
-            print('Encoding finished\n')
-        else:
-            raise AttributeError('Unknown SNN variant.')
+        # TODO Encoding for fast version must be included
 
     def load(self):
         # Dtype conversion necessary because layers use float32 by default
         # .astype('float32') removed because already included in dataset creation
         self.x_train = np.load(self.dataset_folder + "train_features.npy")  # data training
-        self.x_test = np.load(self.dataset_folder + "test_features.npy")  # data testing
+        self.feature_names_all = np.load(self.dataset_folder + "feature_names.npy")
+        y_train = np.load(self.dataset_folder + "train_labels.npy")  # labels training data
 
-        y_train = np.expand_dims(np.load(self.dataset_folder + "train_labels.npy"), axis=-1)  # labels training data
-        y_test = np.expand_dims(np.load(self.dataset_folder + "test_labels.npy"), axis=-1)  # labels testing data
+        # Get the indices of the features that should be used for this dataset
+        if self.features_used is None:
+            self.indices_features = np.array([i for i in range(self.x_train.shape[2])])
+        else:
+            # Don't change the == condition
+            self.indices_features = np.where(np.isin(self.feature_names_all, self.features_used) == True)[0]
+
+        # Reduce the training data to the features that should be used
+        self.x_train = self.x_train[:, :, self.indices_features]
+
+        # Reduce training data / casebase to those examples that have the right case
+        self.x_train = self.x_train[np.where(y_train == self.case), :, :]
+
+        # all equal to case but included for completeness
+        y_train = y_train[np.where(y_train == self.case)]
+
+        y_train = np.expand_dims(y_train, axis=-1)
 
         # Create a encoder, sparse output must be disabled to get the intended output format
         # Added categories='auto' to use future behavior
@@ -53,11 +63,10 @@ class Dataset:
         # Prepare the encoder with training and test labels to ensure all are present
         # The fit-function 'learns' the encoding but does not jet transform the data
         # The axis argument specifies on which the two arrays are joined
-        self.one_hot_encoder = self.one_hot_encoder.fit(np.concatenate((y_train, y_test), axis=0))
+        self.one_hot_encoder = self.one_hot_encoder.fit(np.concatenate((y_train), axis=0))
 
         # Transforms the vector of labels into a one hot matrix
         self.y_train = self.one_hot_encoder.transform(y_train)
-        self.y_test = self.one_hot_encoder.transform(y_test)
 
         ##
         # Safe information about the dataset
@@ -65,10 +74,9 @@ class Dataset:
 
         # length of the first array dimension is the number of examples
         self.num_train_instances = self.x_train.shape[0]
-        self.num_test_instances = self.x_test.shape[0]
 
         # The total sum of examples
-        self.num_instances = self.num_train_instances + self.num_test_instances
+        self.num_instances = self.num_train_instances
 
         # Length of the second array dimension is the length of the time series
         self.time_series_length = self.x_train.shape[1]
@@ -77,23 +85,22 @@ class Dataset:
         self.time_series_depth = self.x_train.shape[2]
 
         # Get the unique classes and the corresponding number
-        self.classes = np.unique(np.concatenate((y_train, y_test), axis=0))
+        self.classes = np.unique(y_train)
         self.num_classes = self.classes.size
 
         # Data
         # 1. dimension: example
         # 2. dimension: time index
         # 3. dimension: array of all channels
-        print('Dataset loaded')
-        print('\tShape of training set:', self.x_train.shape)
-        print('\tShape of test set:', self.x_test.shape, '\n')
+        print('Casebase for case', self.case, 'loaded')
+        print('\tShape:', self.x_train.shape)
 
     # Draw a random pair of instances
-    def draw_pair(self, is_positive, from_test):
+    def draw_pair(self, is_positive):
 
         # Select dataset depending on parameter
-        ds_y = self.y_test if from_test else self.y_train
-        num_instances = self.num_test_instances if from_test else self.num_train_instances
+        ds_y = self.y_train
+        num_instances = self.num_train_instances
 
         # Draw as long as is_positive criterion is not satisfied
         while True:
