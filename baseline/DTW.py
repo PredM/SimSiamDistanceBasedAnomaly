Qualitity measures (localization, condition, failure mode) added in Inference, BCE weighting tests in SNN and Optimizer, some fixes

baseline/DTW.py
@@ -15,6 +15,8 @@ from configuration.Configuration import Configuration
 def execute_dtw(dataset: FullDataset, start_index, end_index):
     score = 0.0
     start_time = time.clock()
+    useOnlyRelevantAttributes = True
+    print("Consider only relevant attributes defined in config.json for each class: ", useOnlyRelevantAttributes)
 
     for i in range(start_index, end_index):
 
@@ -28,8 +30,33 @@ def execute_dtw(dataset: FullDataset, start_index, end_index):
 
             current_train_example = dataset.x_train[current_train_index]
 
-            # calculate the dtw-distance between the two example
-            current_example_distance, _ = fastdtw(current_test_example, current_train_example, dist=euclidean)
+            # calculate the dtw-distance between two examples
+            if useOnlyRelevantAttributes:
+                #current_test_example_masked = np.multiply(current_test_example, dataset.x_train_masking[current_train_index])
+                #current_train_example_masked = np.multiply(current_train_example, dataset.x_train_masking[current_train_index])
+
+                # For each example
+                current_example_distance = 0
+                num_of_features = 0
+                current_example_masking = dataset.x_train_masking[current_train_index] # masking, weights for current class of example
+                # For each attribute, compare local distance
+                for cnt, entry in enumerate(current_example_masking):
+                    # print("entry: ", entry, "current_test_example shape: ", current_test_example.shape)
+                    # If attribute is relevant then compute the local similarity between both
+                    if entry == 1:
+                        # Local distance: between relevant features
+                        curr_attribute_distance, _ = fastdtw(current_test_example[:,cnt], current_train_example[:,cnt],
+                                                      dist=euclidean)
+                        # print("Feature: ", dataset.feature_names_all[i], " Dist: ", curr_attribute_distance)
+                        current_example_distance = current_example_distance + curr_attribute_distance
+                        num_of_features = num_of_features +1
+                # Global distance: weight distance based on number of features:
+                current_example_distance = current_example_distance / num_of_features
+                print(dataset.y_train_strings[current_train_index], ": ", current_example_distance)
+                #current_example_distance, _ = fastdtw(current_test_example_masked, current_train_example_masked,
+                #                                      dist=euclidean)
+            else:
+                current_example_distance, _ = fastdtw(current_test_example, current_train_example, dist=euclidean)
 
             # save the current example if it's distance is the smaller than the current best
             if current_example_distance < distance_min:
@@ -41,7 +68,7 @@ def execute_dtw(dataset: FullDataset, start_index, end_index):
 
         # print results over all processed test examples
         current_example = i - start_index + 1
-        print('Current example:', current_example, 'Current score:', score, 'Current accuarcy:',
+        print('Current example:', current_example,'Class:', dataset.y_test_strings[i],'Predicted:',dataset.y_train_strings[index_min],'Min distance: ', distance_min, 'Current score:', score, 'Current accuarcy:',
               score / current_example)
 
     elapsed_time = time.clock() - start_time
@@ -65,8 +92,9 @@ def main():
     dataset.load()
 
     # execute for all the total test data set
-    start_index = 0
+    start_index = 3000
     end_index = dataset.num_test_instances
+    print(dataset.y_test_strings[3300])
 
     print('Executing DTW for example ', start_index, ' to ', end_index, 'of the test data set in\n',
           config.training_data_folder, '\n')

data_processing/DatasetCreation.py
@@ -353,26 +353,26 @@ def main():
     print('\nSave to np arrays in ' + config.training_data_folder)
 
     print('Step 1/7')
-    np.save(config.training_data_folder + 'train_features_2_.npy', x_train)
+    np.save(config.training_data_folder + 'train_features_4_.npy', x_train)
     print('Step 2/7')
-    np.save(config.training_data_folder + 'test_features_2_.npy', x_test)
+    np.save(config.training_data_folder + 'test_features_4_.npy', x_test)
     print('Step 3/7')
-    np.save(config.training_data_folder + 'train_labels_2_.npy', y_train)
+    np.save(config.training_data_folder + 'train_labels_4_.npy', y_train)
     print('Step 4/7')
-    np.save(config.training_data_folder + 'test_labels_2_.npy', y_test)
+    np.save(config.training_data_folder + 'test_labels_4_.npy', y_test)
     print('Step 5/7')
-    np.save(config.training_data_folder + 'feature_names_2_.npy', attributes)
+    np.save(config.training_data_folder + 'feature_names_4_.npy', attributes)
     print('Step 6/6')
-    np.save(config.training_data_folder + 'train_failure_times_2_.npy',
+    np.save(config.training_data_folder + 'train_failure_times_4_.npy',
             failure_times_train)  # Contains the associated time of a failure (if not no failure) for each example
     print('Step 7/7')
-    np.save(config.training_data_folder + 'test_failure_times_2_.npy',
+    np.save(config.training_data_folder + 'test_failure_times_4_.npy',
             failure_times_test)
     print('Step 7/7')
-    np.save(config.training_data_folder + 'train_window_times_3.npy',
+    np.save(config.training_data_folder + 'train_window_times_4_.npy',
             window_times_train)  # Contains the start and end time stamp for each training example
     print('Step 7/7')
-    np.save(config.training_data_folder + 'test_window_times_3.npy',
+    np.save(config.training_data_folder + 'test_window_times_4_.npy',
             window_times_test)
 
 

data_processing/TimeSeriesFeatureExtraction.py
@@ -66,25 +66,25 @@ def main():
     df_labels = pd.DataFrame(data=y_train_strings)
     print("TS Fresh Feature Extraction started at: ", datetime.datetime.now())
     extracted_features = extract_features(df_timeSeries_container, column_id="id", column_sort="time")
-    extracted_features.to_pickle(config.case_base_folder +'extractedFeatures_X_caseBase_unfiltered.pkl')
+    extracted_features.to_pickle(config.case_base_folder +'extractedFeatures_X_caseBase_unfiltered_4ms4sec.pkl')
 
     #extracted_features.to_csv('extractedFeatures_X_caseBase_unfiltered.csv', sep=',', encoding='WINDOWS-1252')
     print('extracted features size unfiltered: ', extracted_features.shape)
 
     from tsfresh.utilities.dataframe_functions import impute
     # Remove NANs
-    impute(extracted_features)
+    extracted_features = impute(extracted_features)
     print('extracted features size after impute: ', extracted_features.shape)
 
     from tsfresh import select_features
     X_filtered = select_features(extracted_features, y_train_strings)
     print('filtered features size: ', X_filtered.shape)
     print('filtered features: ', X_filtered)
-    X_filtered.to_pickle(config.case_base_folder +'extractedFeatures_X_filtered.pkl')
+    X_filtered.to_pickle(config.case_base_folder +'extractedFeatures_X_filtered_4ms4sec.pkl')
 
     y_train_strings = np.squeeze(y_train_strings)
     print("y_train_strings: ", y_train_strings.shape)
-    X = pd.read_pickle(config.case_base_folder +'extractedFeatures_X_caseBase_unfiltered.pkl')
+    X = pd.read_pickle(config.case_base_folder +'extractedFeatures_X_caseBase_unfiltered_4ms4sec.pkl')
 
     print("X shape: ", X.shape)
 
@@ -94,81 +94,6 @@ def main():
     #X = impute(X)
     print('extracted features size after impute: ', X.shape)
     #print(np.unique(y_train_strings))
-
-    from tsfresh import select_features
-    X_filtered = select_features(X, y_train_strings)
-    print('filtered features size: ', X_filtered.shape)
-    print('filtered features: ', X_filtered)
-    X_filtered.to_pickle(config.case_base_folder +'extractedFeatures_X_filtered.pkl')
-    print("TS Fresh Feature Extraction finished at: ", datetime.datetime.now())
-    '''
-    # Select Labels to analyze
-    labels_to_analyze = ['txt15_m1_t1_high_wear','txt15_m1_t1_low_wear','txt15_m1_t2_wear',
-                               "no_failure", "txt16_m3_t1_high_wear", "txt16_m3_t1_low_wear"]
-
-    for i, failureLabel in enumerate(labels_to_analyze):
-        # Get indexes to delete:
-        if i == 0:
-            indexesToUse = np.where(y_train_strings == failureLabel)
-        else:
-            indexesToUse = np.append(indexesToUse, np.where(y_train_strings == failureLabel))
-
-    print("Indexes to use: ", indexesToUse, "used: ", len(indexesToUse))
-    X_npy = X.values[indexesToUse, :]  # np.delete(features, indexesToExtract, 0)
-    y_train_strings = y_train_strings[indexesToUse]  # np.delete(labels, indexesToExtract, 0)
-
-    print("X shape: ", X_npy.shape, " Labels shape: ", y_train_strings.shape)
-    headers = X.dtypes.index
-    headers = headers.values
-    X = pd.DataFrame(X_npy, columns = headers)
-    X['Label'] = y_train_strings
-    #print(X[['2__kurtosis', 'Label']].to_string())
-    #print(X[['3__kurtosis','Label']].to_string())
-    #print(X[['1__kurtosis','Label']].to_string())
-    test = X[['2__kurtosis','4__kurtosis', '6__kurtosis','Label']]
-    print(test.groupby('Label').median())
-    feature_scores = mutual_info_classif(X_npy, y_train_strings)
-    # for score, fname in sorted(zip(feature_scores, dv.get_feature_names()), reverse=True)[:10]:
-    print('Feature Scores: ',feature_scores)
-
-    resultlist = zip(feature_scores, headers)
-    file = open(config.case_base_folder +'score_feature_file_2.txt', 'w')
-    for score, featureName in sorted(zip(feature_scores, headers), key=lambda x: x[0], reverse=True):
-        print(score, featureName)
-        file.write(str(score) + ' - ' + featureName + '\n')
-        # print('resultlist unsorted: ', set(resultlist))
-    # sorted(resultlist)
-    # print('sorted: ', sorted(resultlist, key=lambda x: x[0],reverse=True))
-    file.close()
-    '''
-    #print('Number of exaples in training data set:', x_train.shape[0])
-    #print('Reducing to', config.examples_per_class, 'examples per class with', len(classes), 'classes')
-
-
-
-    # transform list of values back into an array and safe to file
-    #casebase_labels = np.stack(casebase_labels_list, axis=0)
-    #casebase_features = np.stack(casebase_features_list, axis=0)
-    #casebase_failures =  np.stack(casebase_failures_list, axis=0)
-    #casebase_windowtimes =  np.stack(casebase_windowtimes_list, axis=0)
-
-    #print('Number of exaples in training data set:', casebase_features.shape[0])
-    '''
-    np.save(config.case_base_folder + 'train_features.npy', casebase_features.astype('float32'))
-    np.save(config.case_base_folder + 'train_labels.npy', casebase_labels)
-    np.save(config.case_base_folder + 'train_failure_times.npy', casebase_failures)
-    np.save(config.case_base_folder + 'train_window_times.npy', casebase_windowtimes)
-
-    # in order for the dataset object to be created, there must also be files for test data in the folder,
-    # even if these are not used for live processing.
-    y_test = np.load(config.training_data_folder + 'test_labels.npy')  # labels of the training data
-    x_test = np.load(config.training_data_folder + 'test_features.npy')  # labels of the training data
-    np.save(config.case_base_folder + 'test_features.npy', x_test.astype('float32'))
-    np.save(config.case_base_folder + 'test_labels.npy', y_test)
-
-    # also copy the file that stores the names of the features
-    np.save(config.case_base_folder + 'feature_names.npy', feature_names)
-    '''
 
 # this script is used to reduce the training data to a specific amount of examples per class
 # to use during live classification because using all examples is not efficient enough

neural_network/BasicNeuralNetworks.py
@@ -207,6 +207,13 @@ class CNNWithClassAttention(NN):
         caseDependentVectorInput = tf.keras.Input(self.input_shape[1], name="Input1")
         layers = self.hyper.cnn2d_layers
 
+        # Create a matrix based on masking vectors for using it as "attention"-like in ABCNN
+        caseDependentVectorInput_softmax = tf.keras.layers.Softmax()(caseDependentVectorInput)
+        caseDependentMatrixInput = tf.tile(caseDependentVectorInput_softmax,[1,self.input_shape[0][0]])
+        reshape = tf.keras.layers.Reshape((self.input_shape[0][0], self.input_shape[0][1]))
+        caseDependentMatrixInput = reshape(caseDependentMatrixInput)
+
+
         if len(layers) < 1:
             print('CNN encoder with less than one layer for 2d kernels is not possible')
             sys.exit(1)
@@ -233,6 +240,11 @@ class CNNWithClassAttention(NN):
                 temp = tf.expand_dims(temp,-1)
                 sensorDataInput2 = tf.concat([sensorDataInput, temp], axis=3)
 
+                # Add ABCNN matrix from beginning
+
+                caseDependentMatrixInput = tf.expand_dims(caseDependentMatrixInput,-1)
+                sensorDataInput2 = tf.concat([sensorDataInput2, caseDependentMatrixInput], axis=3)
+
                 x = conv_layer1(sensorDataInput2)
             else:
                 conv_layer = tf.keras.layers.Conv2D(filters=num_filter, padding='VALID',
@@ -252,7 +264,24 @@ class CNNWithClassAttention(NN):
         # reshape necessary to provide a 3d instead of 4 dim for the FFNN or 1D Conv operations on top
         reshape = tf.keras.layers.Reshape((x.shape[1], x.shape[2]))
         x = reshape(x)
+        '''
+        y = reshape(x)
 
+        conv_layer1d_2 = tf.keras.layers.Conv1D(filters=61, padding='VALID', kernel_size=1,
+                                              strides=1)
+
+        temp = conv_layer1d_2(y)
+        temp = tf.expand_dims(temp, -1)
+        sensorDataInput_fin = tf.concat([x, temp], axis=3)
+
+        conv_layer = tf.keras.layers.Conv2D(filters=1, padding='VALID',
+                                            kernel_size=([1,1]),
+                                            strides=1)
+        x = conv_layer(sensorDataInput_fin)
+
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.ReLU()(x)
+        '''
         if len(layers) < 1:
             print('Attention: no one 1d conv on top of 2d conv is used!')
             sys.exit(1)
@@ -269,21 +298,54 @@ class CNNWithClassAttention(NN):
             x = tf.keras.layers.BatchNormalization()(x)
             x = tf.keras.layers.ReLU()(x)
 
-        x = tf.keras.layers.Dropout(rate=self.hyper.dropout_rate)(x)
+        #x = tf.keras.layers.Dropout(rate=self.hyper.dropout_rate)(x)
 
-        x = tf.keras.layers.Multiply()([x, caseDependentVectorInput])
+        # Implementation of a Feature-wise Modulation
+        # FILM conditioning layer of E. Perez, F. Strub, H. De Vries, V. Dumoulin, and A. Courville.
+        # Film: Visual reasoning with a general conditioning layer. In AAAI, 2018.
 
-        self.model = tf.keras.Model(inputs=[sensorDataInput, caseDependentVectorInput], outputs=x)
-        # Add: softmax
-        # Multiply: dense_1
-        #self.intermediate_layer_model = tf.keras.Model(inputs=caseDependentVectorInput,
-        #                                              outputs=self.model.get_layer("tf_op_layer_Reshape").output)
+        # FiLM Generator
+        '''
+        #beta = tf.keras.layers.Dense(61, activation='tanh')(caseDependentVectorInput)
+        beta = tf.keras.layers.Dropout(rate=self.hyper.dropout_rate)(caseDependentVectorInput)
+        beta = tf.keras.layers.Softmax()(beta)
+        #gamma = tf.keras.layers.Dense(61, activation='tanh')(caseDependentVectorInput)
+        gamma = tf.keras.layers.Dropout(rate=self.hyper.dropout_rate)(caseDependentVectorInput)
+        gamma = tf.keras.layers.Softmax()(gamma)
+        gamma = tf.math.add(gamma, 1)
 
+        # FiLM of last layer
+        x = tf.keras.layers.Multiply()([x, gamma])
+        x = tf.keras.layers.Add()([x, beta])
+        '''
+        #gates = tf.keras.layers.Dense(61, activation='relu')(caseDependentVectorInput)
+        #gates = tf.keras.layers.Dense(61, activation='relu')(gates)
+        #gates = tf.keras.layers.Softmax()(gates)
+        #gates = tf.math.add(gates,1)
+        #gates = tf.identity(gates)
+
+        #x = tf.keras.layers.Multiply()([x, gates])
+        '''
+        x = tf.keras.layers.Flatten()(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.Dense(units=1024, activation=tf.keras.activations.relu)(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.Dense(units=768, activation=tf.keras.activations.relu)(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.Dense(units=512, activation=tf.keras.activations.relu)(x)
+
+        #reshape = tf.keras.layers.Reshape((x.shape[1], x.shape[2]))
+        x = tf.keras.layers.Reshape((512, 1))(x)
+        '''
+        self.model = tf.keras.Model(inputs=[sensorDataInput, caseDependentVectorInput], outputs=[x,caseDependentVectorInput])
+        '''
+        self.intermediate_layer_model = tf.keras.Model(inputs=caseDependentVectorInput,
+                                                      outputs=self.model.get_layer("softmax").output)
+        '''
         # Query-value attention of shape [batch_size, Tq, filters].
         # print("inputs", inputs)
         # print("x: ", x)
         # input_lastConvLayer_attention_seq = tf.keras.layers.Attention()([x, caseDepVectEmbedding])
-
 
 class CNN1DWithClassAttention(NN):
 

neural_network/Dataset.py
@@ -41,6 +41,7 @@ class Dataset:
         self.x_auxCaseVector_test = None
         self.x_class_label_to_attribute_masking_arr = {}
         self.x_train_masking = None # npy 2d arr with shape (examples,#attributes)
+        self.masking_unique = None
 
         # additional information for each example about their window timeframe and failure occurence time
         self.windowTimes_train = None
@@ -245,30 +246,36 @@ class FullDataset(Dataset):
         self.testArr_label_failureTime_uniq = np.delete(testArr_label_failureTime_uniq, idx, 0)
         self.testArr_label_failureTime_counts = np.delete(testArr_label_failureTime_counts, idx, 0)
 
+        # TODO für CBS auslassen
         # Generate for each label a masking array
         #print("Config relevant features: ", self.config.relevant_features)
         num_of_attributes = self.x_train.shape[2]
-        for label in self.config.relevant_features:
+        self.masking_unique = np.zeros([len(self.config.relevant_features),num_of_attributes ])
+        i=0
+        for class_label in self.classes_total: #[i]self.config.relevant_features
+            #print("Label: ", class_label)
             #print(self.config.relevant_features[label])
             curr_masking_arr = np.zeros([num_of_attributes])
-            for attribute in self.config.relevant_features[label]:
+            #if self.config.relevant_features[class_label] is None:
+            #    print("Relevant attributes for: ", class_label, " missing")
+            for attribute in self.config.relevant_features[class_label]:
                 idx_set_to_one = np.where(self.feature_names_all == attribute)
-                curr_masking_arr[idx_set_to_one] =1
+                curr_masking_arr[idx_set_to_one] =1.0
                 #print(attribute ,": ", np.where(self.feature_names_all == attribute))
             #print(label, ": ", curr_masking_arr)
-            self.x_class_label_to_attribute_masking_arr[label] = curr_masking_arr
+            curr_masking_arr = np.where(curr_masking_arr == 0, 0.0,curr_masking_arr)
+            self.x_class_label_to_attribute_masking_arr[class_label] = curr_masking_arr
+            self.masking_unique[i,:] = curr_masking_arr
+            i = i+1
+            #print(label, ": ", curr_masking_arr)
             #self.x_class_label_to_attribute_masking[label]
         #print("test: ", self.x_class_label_to_attribute_masking_arr['txt18_transport_failure_mode_wout_workpiece'])
 
         # Add the masking array to each corresponding example
         self.x_train_masking = np.zeros([self.x_train.shape[0], num_of_attributes])
         for i in range(self.x_train.shape[0]):
-            #print(i, ": ", self.x_class_label_to_attribute_masking_arr[self.y_train_strings[i]])
+            #print(i, " - ", self.y_train_strings[i],":",self.x_class_label_to_attribute_masking_arr[self.y_train_strings[i]])
             self.x_train_masking[i,:] = self.x_class_label_to_attribute_masking_arr[self.y_train_strings[i]]
-
-
-
-
 
         # data
         # 1. dimension: example
@@ -353,13 +360,15 @@ class FullDataset(Dataset):
         if(notion_of_sim == 'failuremode'):
             pair_label_sim = self.df_label_sim_failuremode.loc[label_1,label_2]
         elif(notion_of_sim == 'localization'):
-            pair_label_sim = self.df_label_sim_failuremode.loc[label_1,label_2]
+            pair_label_sim = self.df_label_sim_localization.loc[label_1,label_2]
         elif (notion_of_sim == 'condition'):
             pair_label_sim = self.df_label_sim_condition.loc[label_1, label_2]
         else:
-            #print("Similarity notion: ", notion_of_sim, " unknown! Results in sim 0")
+            print("Similarity notion: ", notion_of_sim, " unknown! Results in sim 0")
             pair_label_sim = 0
+        #print(label_1, " - ", label_2, ":", str(pair_label_sim))
         return float(pair_label_sim)
+
 # variation of the dataset class that consists only of examples of the same case
 # does not contain test data because this isn't needed on the level of each case
 class CaseSpecificDataset(Dataset):

neural_network/Inference.py
@@ -53,13 +53,19 @@ class Inference:
                                         'Chances': self.dataset.testArr_label_failureTime_counts,
                                         'AsHealth': np.zeros(self.dataset.testArr_label_failureTime_uniq.shape[0]),
                                         'AsOtherFailure': np.zeros(self.dataset.testArr_label_failureTime_uniq.shape[0])})
+        self.quality_all_failure_localization = 0
+        self.quality_all_failure_mode_diagnosis = 0
+        self.quality_all_condition_quality = 0
+        self.quality_failure_localization = 0
+        self.quality_failure_mode_diagnosis = 0
+        self.quality_condition_quality = 0
 
     def infer_test_dataset(self):
         correct, num_infers = 0, 0
         start_time = time.perf_counter()
 
         # print the case embeddings for each class
-        if self.architecture.encoder.hyper.encoder_variant == 'cnn1dwithclassattention':
+        if self.architecture.encoder.hyper.encoder_variant == 'cnnwithclassattention':
             print()
             #self.architecture.print_learned_case_vectors()
         # self.architecture.printLearnedCaseMatrix()
@@ -81,6 +87,7 @@ class Inference:
 
         # infer all examples of the test data set
         example_cnt=0
+        example_cnt_failure = 0
         for idx_test in idx_test_examples_query_pool:
 
             max_sim = 0
@@ -114,9 +121,13 @@ class Inference:
             # if correctly classified increase the true positive field of the correct class and the of all classes
             if real == max_sim_class:
                 correct += 1
+            self.quality_all_condition_quality += self.dataset.getSimBetweenPairLabels(real,max_sim_class,"condition")
+            self.quality_all_failure_mode_diagnosis += self.dataset.getSimBetweenPairLabels(real, max_sim_class, "failuremode")
+            self.quality_all_failure_localization += self.dataset.getSimBetweenPairLabels(real, max_sim_class, "localization")
 
             # Storing the prediction result in respect to a failure occurrence
             if not real == 'no_failure':
+                example_cnt_failure +=1
                 if real == max_sim_class:
                     self.failure_results.loc[(self.failure_results['Label'].isin([real])) & (
                         self.failure_results['FailureTime'].isin(self.dataset.failureTimes_test[idx_test])),'Correct'] += 1
@@ -126,10 +137,18 @@ class Inference:
                 else:
                     self.failure_results.loc[(self.failure_results['Label'].isin([real])) & (
                         self.failure_results['FailureTime'].isin(self.dataset.failureTimes_test[idx_test])), 'AsOtherFailure'] += 1
+                self.quality_condition_quality += self.dataset.getSimBetweenPairLabels(real, max_sim_class,
+                                                                                           "condition")
+                self.quality_failure_mode_diagnosis += self.dataset.getSimBetweenPairLabels(real, max_sim_class,
+                                                                                                "failuremode")
+                self.quality_failure_localization += self.dataset.getSimBetweenPairLabels(real, max_sim_class,
+                                                                                           "localization")
 
             # regardless of the result increase the number of examples with this class
             self.results.loc[real, '#Examples'] =+ 1
             num_infers += 1
+            #catch division by zero:
+            if example_cnt_failure == 0: example_cnt_failure=1
 
             # print result for this example
             example_results = [
@@ -140,6 +159,9 @@ class Inference:
                 ['Similarity:', max_sim],
                 # ['K-nearest Neighbors: ', k_nn_string],
                 ['Current accuracy:', correct / num_infers],
+                ['Diagnosis quality:', self.quality_failure_mode_diagnosis / example_cnt_failure],
+                ['Localization quality:', self.quality_failure_localization / example_cnt_failure],
+                ['Condition quality:', self.quality_condition_quality / example_cnt_failure],
                 ['Query Window:', str(str(self.dataset.windowTimes_test[idx_test][0]).replace("['YYYYMMDD HH:mm:ss (","").replace(")']","") + " - " + str(self.dataset.windowTimes_test[idx_test][2]).replace("['YYYYMMDD HH:mm:ss (","").replace(")']",""))],
                 ['Query Failure:', str(self.dataset.failureTimes_test[idx_test])]
             ]
@@ -226,6 +248,11 @@ class Inference:
                                         failure_detected_AsOtherFailure_sum]
 
         print(self.failure_results.to_string())
+        print('Self-defined quality measures: ')
+        print(""+"\t")
+        print('Diagnosis quality all:',"\t\t", self.quality_all_failure_mode_diagnosis / num_infers,"\t\t Failure only: \t", self.quality_failure_mode_diagnosis / example_cnt_failure)
+        print('Localization quality:',"\t\t", self.quality_all_failure_localization / num_infers,"\t\t Failure only: \t", self.quality_failure_localization / example_cnt_failure)
+        print('Condition quality:',"\t\t", self.quality_all_condition_quality / num_infers,"\t\t Failure only: \t", self.quality_condition_quality / example_cnt_failure)
 
 
 def main():

neural_network/Optimizer.py
@@ -14,6 +14,7 @@ from case_based_similarity.CaseBasedSimilarity import CBS, SimpleCaseHandler
 from configuration.Configuration import Configuration
 from configuration.Hyperparameter import Hyperparameters
 from neural_network.BasicNeuralNetworks import TCN
+import tensorflow.keras.backend as K
 
 from neural_network.Dataset import FullDataset
 from neural_network.Inference import Inference
@@ -68,7 +69,15 @@ class Optimizer:
 
             # Calculate the loss and the gradients
             if self.config.type_of_loss_function == "binary_cross_entropy":
-                loss = tf.keras.losses.binary_crossentropy(y_true=true_similarities, y_pred=pred_similarities)
+                if self.config.use_margin_reduction_based_on_label_sim:
+                    sim = self.getSimiliarityBetweenTwoLabelStringForNegPairSampleWeightInBCE(query_classes)
+                    #print('query_classes', query_classes)
+                    #print('sim', sim)
+                    #bce = tf.keras.losses.BinaryCrossentropy()
+                    #loss = bce(y_true=true_similarities, y_pred=pred_similarities, sample_weight=sim[0])
+                    loss = self.weighted_binary_crossentropy(y_true=true_similarities, y_pred=pred_similarities, weight=sim)
+                else:
+                    loss = tf.keras.losses.binary_crossentropy(y_true=true_similarities, y_pred=pred_similarities)
             elif self.config.type_of_loss_function == "constrative_loss":
                 if self.config.use_margin_reduction_based_on_label_sim:
                     pairwiseLabelSimiliarity = self.getSimiliarityBetweenTwoLabelString(query_classes)
@@ -106,15 +115,15 @@ class Optimizer:
         margin_square = tf.square(tf.maximum(margin - y_pred, 0))
         return tf.keras.backend.mean(y_true * square_pred + (1 - y_true) * margin_square)
 
-    def contrastive_loss_mod(self, y_true, y_pred):
-        """
-        Contrastive loss from Hadsell-et-al.'06
-        http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
-        """
-        margin = self.config.margin_of_loss_function
-        square_pred = tf.square(y_pred)
-        margin_square = tf.maximum(tf.square(margin) - tf.square(y_pred), 0)
-        return tf.keras.backend.mean((1 - y_true) * square_pred + y_true * margin_square)
+    def weighted_binary_crossentropy(self, y_true, y_pred, weight=1.):
+        '''
+        Weighted BCE that smoothes only the wrong example according to interclass similarities
+        '''
+        y_true = K.clip(tf.convert_to_tensor(y_true,dtype=tf.float32), K.epsilon(), 1 - K.epsilon())
+        y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
+        #org: logloss = -(y_true * K.log(y_pred) * weight + (1 - y_true) * K.log(1 - y_pred))
+        logloss = -(y_true * K.log(y_pred) + (1 - y_true + (weight/2)) * K.log(1 - y_pred))
+        return K.mean(logloss, axis=-1)
 
     def getSimiliarityBetweenTwoLabelString(self, classes):
         # Returns the similarity between 2 failures (labels) in respect to the location of occurrence,
@@ -129,6 +138,34 @@ class Optimizer:
             sim = (self.dataset.getSimBetweenPairLabels(a,b,"condition")
                    + self.dataset.getSimBetweenPairLabels(a,b,"localization")
                    + self.dataset.getSimBetweenPairLabels(a,b,"failuremode"))/3
+            pairwise_class_label_sim[pair_index] = sim
+            #print("pairwise_class_label_sim: ", sim)
+
+        return pairwise_class_label_sim
+
+    def getSimiliarityBetweenTwoLabelStringForNegPairSampleWeightInBCE(self, classes):
+        # Returns the similarity between 2 failures (labels) in respect to the location of occurrence,
+        # type of failure (failure mode) and the condition of the data sample.
+        # Implemented for use as label smoother of neg pair in BCE
+        # Input: 1d npy array with pairwise class labels as strings [2*batchsize]
+        # Output: 1d npy [batchsize]
+        pairwise_class_label_sim = np.zeros([len(classes)//2])
+        for pair_index in range(len(classes)//2):
+            a = classes[2 * pair_index]
+            b = classes[2 * pair_index + 1]
+            #print("pair_index: ", pair_index, "a: ", a ," b: ",b)
+
+            sim = (self.dataset.getSimBetweenPairLabels(a,b,"condition")
+                   + self.dataset.getSimBetweenPairLabels(a,b,"localization")
+                   + self.dataset.getSimBetweenPairLabels(a,b,"failuremode"))/3
+
+            #sim = self.dataset.getSimBetweenPairLabels(a, b, "failuremode")
+
+            # Transform similarity to a appropriate weight
+            #print("Sim before: ", sim)
+            if sim < 1 : sim =1-sim
+            #print("Sim after: ", sim)
+
             pairwise_class_label_sim[pair_index] = sim
             #print("pairwise_class_label_sim: ", sim)
 
@@ -239,7 +276,16 @@ class SNNOptimizer(Optimizer):
             batch_pairs_indices.append(neg_pair[0])
             batch_pairs_indices.append(neg_pair[1])
             #print("Negpair idx: ", self.dataset.y_train_strings[neg_pair[0]], " - ",self.dataset.y_train_strings[neg_pair[1]])
-            batch_true_similarities.append(0.0)
+            if self.config.use_sim_value_for_neg_pair:
+                class_string_a = self.dataset.y_train_strings[neg_pair[0]]
+                class_string_b = self.dataset.y_train_strings[neg_pair[1]]
+                sim = (self.dataset.getSimBetweenPairLabels(class_string_a, class_string_b, "condition")
+                       + self.dataset.getSimBetweenPairLabels(class_string_a, class_string_b, "localization")
+                       + self.dataset.getSimBetweenPairLabels(class_string_a, class_string_b, "failuremode")) / 3
+                batch_true_similarities.append(sim)
+            else:
+                batch_true_similarities.append(0.0)
+
             batch_pairs_indices_firstUsedforSecond.append(neg_pair[0])
             batch_pairs_indices_firstUsedforSecond.append(neg_pair[0])
 
@@ -272,7 +318,7 @@ class SNNOptimizer(Optimizer):
             #print("model_input: ", model_input.shape)
 
             batch_loss = self.update_single_model([model_input, model_input2], true_similarities, self.architecture,
-                                                  self.adam_optimizer, self.architecture.hyper.gradient_cap)
+                                                  self.adam_optimizer, self.architecture.hyper.gradient_cap, query_classes=model_input_class_strings)
         elif self.architecture.hyper.encoder_variant == 'cnn2d':
             model_input = np.reshape(model_input,
                                      (model_input.shape[0], model_input.shape[1], model_input.shape[2], 1))

neural_network/SNN.py
@@ -97,7 +97,8 @@ class SimpleSNN(AbstractSimilarityMeasure):
                     auxiliaryInput[2 * i] = self.dataset.x_class_label_to_attribute_masking_arr[self.dataset.y_train_strings[index + i]]
                     # np.zeros(self.dataset.x_auxCaseVector_train.shape[1])
                     # self.dataset.x_auxCaseVector_train[index]
-                    auxiliaryInput[2 * i + 1] = self.dataset.x_class_label_to_attribute_masking_arr[self.dataset.y_train_strings[index + i]]
+                    #np.zeros(self.dataset.x_class_label_to_attribute_masking_arr[self.dataset.y_train_strings[index + i]].shape[0])
+                    auxiliaryInput[2 * i + 1] =  self.dataset.x_class_label_to_attribute_masking_arr[self.dataset.y_train_strings[index + i]]
 
             if self.hyper.encoder_variant == 'cnn1dwithclassattention':
                 input_pairs = input_pairs.reshape((input_pairs.shape[0],input_pairs.shape[1],input_pairs.shape[2]))
@@ -118,7 +119,6 @@ class SimpleSNN(AbstractSimilarityMeasure):
     # get the similarities of the example to each example in the dataset
     # used for inference
     def get_sims(self, example):
-
         # Splitting the batch size for inference in the case of using a TCN with warping FFNN due to GPU memory issues
         if type(self.encoder) == TCN or self.config.use_batchsize_for_inference_sim_calculation:
             return self.get_sims_in_batches(example)
@@ -165,7 +165,6 @@ class SimpleSNN(AbstractSimilarityMeasure):
 
     @tf.function
     def get_sims_batch(self, batch):
-        # print("get_sims_batch: batch shape: ", batch, "self.hyper.encoder_variant: ", self.hyper.encoder_variant)
 
         # calculate the output of the subnet for the examples in the batch
         context_vectors = self.encoder.model(batch, training=self.training)
@@ -192,49 +191,72 @@ class SimpleSNN(AbstractSimilarityMeasure):
 
     @tf.function
     def get_sim_pair(self, context_vectors, pair_index):
-        # if a concat layer is used in the cnn1dclassattention, then context vectors need to be reshaped from 2d to 3d
+        # TODO: Reminder if a concat layer is used in the cnn1dclassattention,
+        #  then context vectors need to be reshaped from 2d to 3d (implement directly in BasicNeuralNetorks)
         # context_vectors = tf.reshape(context_vectors,[context_vectors.shape[0],context_vectors.shape[1],1])
 
-        a = context_vectors[2 * pair_index, :, :]
-        b = context_vectors[2 * pair_index + 1, :, :]
-        # print("a.shape: ", a.shape)
+        # Parsing the input (e.g., two 1d or 2d vectors depending on which encoder is used) to calculate distance / sim
+        if self.encoder.hyper.encoder_variant == 'cnnwithclassattention':
+            # Output of encoder are encoded time series and additional things e.g., weights vectors
+            a = context_vectors[0][2 * pair_index, :, :]
+            b = context_vectors[0][2 * pair_index + 1, :, :]
+            a_weights = context_vectors[1][2 * pair_index, :]
+            b_weights = context_vectors[1][2 * pair_index+1, :]
+        else:
+            a = context_vectors[2 * pair_index, :, :]
+            b = context_vectors[2 * pair_index + 1, :, :]
+
 
         # TODO Implement as method call and fix in fast version
         #  tf.exp() was added here directly
 
-        # simple similarity measure:
+        # simple similarity measures:
         if self.config.simple_Distance_Measure == "abs_mean":
             # mean of absolute difference / Manhattan Distance ?
             diff = tf.abs(a - b)
             distance_example = tf.reduce_mean(diff)
             sim_example = tf.exp(-distance_example)
+
         elif self.config.simple_Distance_Measure == "euclidean_sim":
             # Euclidean distance converted as similarity
-            diff = tf.norm(a - b, ord='euclidean')
+            if self.config.useFeatureWeightedSimilarity:
+                diff = tf.norm(a - b, ord='euclidean', axis=0, keepdims=True)
+                # include the weights to influence overall distance
+                a_weights = tf.dtypes.cast(a_weights, tf.float32)
+                a_weights = tf.dtypes.cast(a_weights, tf.float32)
+                a_weights_sum = tf.reduce_sum(a_weights)
+                a_weights = a_weights / a_weights_sum
+                diff = tf.reduce_sum(tf.abs(diff * a_weights))
+            else:
+                diff = tf.norm(a - b, ord='euclidean')
             sim_example = 1 / (1 + tf.reduce_sum(diff))
+
         elif self.config.simple_Distance_Measure == "euclidean_dis":
-            # Euclidean distance
-            # flatten in case of a matrix (e.g., 1d cnn output)
-            #print(a.shape[0])
-            #a = tf.reshape(a.shape[0]*a.shape[1])
-            #print(a.shape)
-            #a = tf.squeeze(a)
-            #b = tf.squeeze(b)
-            #diff = tf.linalg.norm(a - b, axis=1)
-            diff = tf.norm(a - b, ord='euclidean')
-            print(diff.shape)
-            print("current diff: ",diff)
+            # Euclidean distance (required in constrative loss function)
+            if self.config.useFeatureWeightedSimilarity:
+                diff = tf.norm(a - b, ord='euclidean', axis=0, keepdims=True)
+                # include the weights to influence overall distance
+                a_weights = tf.dtypes.cast(a_weights, tf.float32)
+                a_weights_sum = tf.reduce_sum(a_weights)
+                a_weights = a_weights / a_weights_sum
+                diff = tf.reduce_sum(tf.abs(diff * a_weights))
+                diff = tf.reduce_sum(diff)
+            else:
+                diff = tf.norm(a - b, ord='euclidean')
             sim_example = diff
+
         elif self.config.simple_Distance_Measure == "dot_product":
             # dot product
             sim = tf.matmul(a, b, transpose_b=True)
             sim_example = tf.reduce_mean(sim)
+
         elif self.config.simple_Distance_Measure == "cosine":
             # cosine, source: https://stackoverflow.com/questions/43357732/how-to-calculate-the-cosine-similarity-between-two-tensors/43358711
             normalize_a = tf.nn.l2_normalize(a, 0)
             normalize_b = tf.nn.l2_normalize(b, 0)
             cos_similarity = tf.reduce_sum(tf.multiply(normalize_a, normalize_b))
             sim_example = cos_similarity
+
         elif self.config.simple_Distance_Measure == "jaccard":
             # Prüfen, source: https://stackoverflow.com/questions/43261072/jaccards-distance-matrix-with-tensorflow
             tp = tf.reduce_sum(tf.multiply(a, b), 1)
@@ -248,7 +270,11 @@ class SimpleSNN(AbstractSimilarityMeasure):
     def print_learned_case_vectors(self, num_of_max_pos=5):
         # this methods prints the learned case embeddings for each class and its values
         cnt = 0
-        case_embeddings = self.encoder.intermediate_layer_model(self.dataset.classes_Unique_oneHotEnc,
+        #print(self.dataset.masking_unique.shape)
+        for i in range(len(self.dataset.feature_names_all)):
+            print(i,": ", self.dataset.feature_names_all[i])
+        input= self.dataset.masking_unique
+        case_embeddings = self.encoder.intermediate_layer_model(input,
                                                                 training=self.training)
         # Get positions with maximum values
         max_pos = np.argsort(-case_embeddings, axis=1)  # [::-1]  # np.argmax(case_embeddings, axis=1)
@@ -259,8 +285,8 @@ class SimpleSNN(AbstractSimilarityMeasure):
                 row = np.array(case_embeddings[cnt, :])
                 maxNPos = max_pos[cnt, :num_of_max_pos]
                 minNPos = min_pos[cnt, :num_of_max_pos]
-                print(i, " ", self.dataset.classes_Unique_oneHotEnc[cnt], "Max Filter:", max_pos[cnt, :5],
-                      "Max Values: ", row[maxNPos], "Min Values: ", row[minNPos])
+                print(i, " ", input[cnt], " | Max Filter: ", max_pos[cnt, :5], " | Min Filter: ", min_pos[cnt, :5],
+                      " | Max Values: ", row[maxNPos], " | Min Values: ", row[minNPos])
                 cnt = cnt + 1
 
     def print_learned_case_matrix(self):