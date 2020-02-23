- Fixed CaseBaseExtraction Script: Now copys all unchanged but necassary files automatically - Fixed gradient cap bug. If no gradient cap should be used the variable value now must be <0, changed all json configs accordingly

data_processing/CaseBaseExtraction.py
@@ -2,6 +2,7 @@ import sys
 import os
 import numpy as np
 from numpy.random.mtrand import RandomState
+from shutil import copyfile
 
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
 
@@ -29,7 +30,7 @@ def main():
         indices_of_classes.append(np.where(y_train == c)[0])
 
     # reduce classes to equal many examples
-    new_indcies = []
+    new_indices = []
     ran = RandomState(config.random_seed_index_selection)
     for i in range(len(indices_of_classes)):
         length = len(indices_of_classes[i])
@@ -40,42 +41,54 @@ def main():
         temp = ran.choice(indices_of_classes[i], epc, replace=False)
         # print(len(indices_of_classes[i]), len(temp))
 
-        new_indcies.append(temp)
+        new_indices.append(temp)
 
     casebase_features_list = []
     casebase_labels_list = []
     casebase_failures_list = []
-    casebase_windowtimes_list = []
+    casebase_window_times_list = []
 
     # extract the values at the selected indices and add to list
     for i in range(len(classes)):
-        casebase_labels_list.extend(y_train[new_indcies[i]])
-        casebase_features_list.extend(x_train[new_indcies[i]])
-        casebase_failures_list.extend(failureTimes_train[new_indcies[i]])
-        casebase_windowtimes_list.extend(windowTimes_train[new_indcies[i]])
+        casebase_labels_list.extend(y_train[new_indices[i]])
+        casebase_features_list.extend(x_train[new_indices[i]])
+        casebase_failures_list.extend(failureTimes_train[new_indices[i]])
+        casebase_window_times_list.extend(windowTimes_train[new_indices[i]])
 
     # transform list of values back into an array and safe to file
     casebase_labels = np.stack(casebase_labels_list, axis=0)
     casebase_features = np.stack(casebase_features_list, axis=0)
     casebase_failures = np.stack(casebase_failures_list, axis=0)
-    casebase_windowtimes = np.stack(casebase_windowtimes_list, axis=0)
+    casebase_window_times = np.stack(casebase_window_times_list, axis=0)
 
     print('Number of exaples in training data set:', casebase_features.shape[0])
 
     np.save(config.case_base_folder + 'train_features.npy', casebase_features.astype('float32'))
     np.save(config.case_base_folder + 'train_labels.npy', casebase_labels)
     np.save(config.case_base_folder + 'train_failure_times.npy', casebase_failures)
-    np.save(config.case_base_folder + 'train_window_times.npy', casebase_windowtimes)
+    np.save(config.case_base_folder + 'train_window_times.npy', casebase_window_times)
 
     # in order for the dataset object to be created, there must also be files for test data in the folder,
     # even if these are not used for live processing.
-    y_test = np.load(config.training_data_folder + 'test_labels.npy')  # labels of the training data
-    x_test = np.load(config.training_data_folder + 'test_features.npy')  # labels of the training data
-    np.save(config.case_base_folder + 'test_features.npy', x_test.astype('float32'))
-    np.save(config.case_base_folder + 'test_labels.npy', y_test)
+    # y_test = np.load(config.training_data_folder + 'test_labels.npy')  # labels of the test data
+    # x_test = np.load(config.training_data_folder + 'test_features.npy')  # features of the test data
+    # test_window_times = np.load(config.training_data_folder + 'test_window_times.npy')
+    # test_failure_times = np.load(config.training_data_folder + 'test_failure_times.npy')
+    #
+    # np.save(config.case_base_folder + 'test_features.npy', x_test.astype('float32'))
+    # np.save(config.case_base_folder + 'test_labels.npy', y_test)
+    # np.save(config.case_base_folder + 'test_window_times.npy', test_window_times)
+    # np.save(config.case_base_folder + 'test_failure_times.npy', test_failure_times)
+    #
+    # # also copy the file that stores the names of the features
+    # np.save(config.case_base_folder + '', feature_names)
 
-    # also copy the file that stores the names of the features
-    np.save(config.case_base_folder + 'feature_names.npy', feature_names)
+    files_to_copy = ['feature_names.npy', 'test_labels.npy', 'test_features.npy', 'test_window_times.npy',
+                     'test_failure_times.npy', 'FailureMode_Sim_Matrix.csv', 'Lokalization_Sim_Matrix.csv',
+                     'Condition_Sim_Matrix.csv']
+
+    for file in files_to_copy:
+        copyfile(config.training_data_folder + file, config.case_base_folder + file)
 
 
 # this script is used to reduce the training data to a specific amount of examples per class

neural_network/Optimizer.py
@@ -97,14 +97,8 @@ class Optimizer:
 
             grads = tape.gradient(loss, trainable_params)
 
-            # Maybe change back to clipnorm = self.hyper.gradient_cap in adam initialisation
-            if gradient_cap == 0:
-                clipped_grads = grads
-            else:
-                clipped_grads = tf.clip_by_global_norm(grads, gradient_cap)
-
             # Apply the gradients to the trainable parameters
-            optimizer.apply_gradients(zip(clipped_grads, trainable_params))
+            optimizer.apply_gradients(zip(grads, trainable_params))
 
             return loss
 
@@ -187,11 +181,16 @@ class SNNOptimizer(Optimizer):
     def __init__(self, architecture, dataset, config):
 
         super().__init__(architecture, dataset, config)
-        self.adam_optimizer = tf.keras.optimizers.Adam(learning_rate=self.architecture.hyper.learning_rate)
         self.train_loss_results = []
-        self.best_loss = 1000  #
+        self.best_loss = 1000
         self.stopping_step_counter = 0
         self.dir_name_last_model_saved = None
+
+        if self.architecture.hyper.gradient_cap >= 0:
+            self.adam_optimizer = tf.keras.optimizers.Adam(learning_rate=self.architecture.hyper.learning_rate,
+                                                           clipnorm=self.architecture.hyper.gradient_cap)
+        else:
+            self.adam_optimizer = tf.keras.optimizers.Adam(learning_rate=self.architecture.hyper.learning_rate)
 
     def optimize(self):
         current_epoch = 0
@@ -201,9 +200,8 @@ class SNNOptimizer(Optimizer):
             current_epoch = self.architecture.hyper.epochs_current
 
             if current_epoch >= self.architecture.hyper.epochs:
-                print(
-                    'Training already finished. If training should be continued'
-                    ' increase the number of epochs in the hyperparameter file of the safed model')
+                print('Training already finished. If training should be continued'
+                      ' increase the number of epochs in the hyperparameter file of the safed model')
             else:
                 print('Continuing the training at epoch', current_epoch)
 
@@ -423,8 +421,14 @@ class CBSOptimizer(Optimizer, ABC):
         for case_handler in self.architecture.case_handlers:
             self.losses[case_handler.dataset.case] = []
             self.goal_epochs[case_handler.dataset.case] = case_handler.hyper.epochs
-            self.optimizer[case_handler.dataset.case] = tf.keras.optimizers.Adam(
-                learning_rate=case_handler.hyper.learning_rate)
+
+            if case_handler.hyper.gradient_cap >= 0:
+                opt = tf.keras.optimizers.Adam(learning_rate=case_handler.hyper.learning_rate,
+                                               clipnorm=case_handler.hyper.gradient_cap)
+            else:
+                opt = tf.keras.optimizers.Adam(learning_rate=case_handler.hyper.learning_rate)
+
+            self.optimizer[case_handler.dataset.case] = opt
 
         self.max_epoch = max(self.goal_epochs.values())
 