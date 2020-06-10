5ce8fd1265868dff44e0ba0af8d6478fd5accf29
Niklas <niklas.weingarz@gmail.com>
2020-06-10 19:27:14 +0200

    Removed some old files

--- archive/allow_memory_growth.py
+++
@@ -1,4 +0,0 @@
-# if True:
-#     physical_devices = tf.config.experimental.list_physical_devices('GPU')
-#     for device in physical_devices:
-#         tf.config.experimental.set_memory_growth(device, True)
